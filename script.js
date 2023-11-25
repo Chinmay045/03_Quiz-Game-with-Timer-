dc2b03a7122aba73a0183875e65962628e528ff3
Chinmay <chinz.sandur07@gmail.com>
2023-11-25 11:12:26 +0530

    add question in the object

---
+++ script.js
@@ -0,0 +1,33 @@
+const questions = [
+
+    {
+        'que': "Which of the following is a markup language",
+        "a": "HTML",
+        "b": "CSS",
+        "c": "JavaScript",
+        "d": "Php",
+        "correct": "a"
+    },
+
+    {
+        'que': "What year was javascript was launched",
+        "a": "1996",
+        "b": "1995",
+        "c": "1994",
+        "d": "None of these",
+        "correct": "b"
+    },
+
+    {
+        'que': "What does CSS stand for",
+        "a": "Hypertext MarkUp Language",
+        "b": "Cascading Style Sheet",
+        "c": "Json Object notation",
+        "d": "Helicopter teminals",
+        "correct": "b"
+    }
+]
+
+
+
+
